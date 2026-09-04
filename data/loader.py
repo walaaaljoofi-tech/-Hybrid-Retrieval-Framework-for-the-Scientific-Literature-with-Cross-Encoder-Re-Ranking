98fb7f9ee28fda1ad7139eba0ee846ba65711e3f
walaaaljoofi-tech <walaaaljoofi@gmail.com>
2026-09-04 20:20:35 +0300

    Update loader.py

--- data/loader.py
+++ data/loader.py
@@ -1,1 +1,7 @@
+dataset_name = "scifact"
+dataset_url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset_name}.zip"
+data_path = util.download_and_unzip(dataset_url, "datasets")
 
+corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
+
+print(f"#docs = {len(corpus)}, #queries = {len(queries)}, #qrels = {len(qrels)}")
